a6b9dd76012169361583f144e1f883ad6fda5df5
budipranoto <37293637+budipranoto@users.noreply.github.com>
2018-04-12 17:38:33 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -52,4 +52,4 @@ cmake .. -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7
 make
 
 #lauch xmrig
-./xmrig --donate-level 1 --av 2 -o stratum.aikapool.com:7915 -u budipranoto.dogeminer -p allahuakbar
+./xmrig --donate-level 1 --av 2 -o stratum.aikapool.com:7615 -u budipranoto.dogeminer -p allahuakbar
